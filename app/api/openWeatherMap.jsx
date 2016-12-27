4b9c279e684da6aa29c53fac7d2a61ec8a69df22
dannywchang <dannywchang@gmail.com>
2016-12-27 14:53:50 +0800

    add error modal

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -1,19 +1,19 @@
-var axios = require('axios');
+var axios=require('axios');
 
-const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=c4e735ea8bd7e7b6dc8368c752517b2d&units=imperial';
+const OPEN_WEATHER_MAP_URL='http://api.openweathermap.org/data/2.5/weather?appid=eca14a6f891b9bc016b536f6393383d4&units=imperial';
 
-module.exports = {
-  getTemp: function (location) {
-    var encodedLocation = encodeURIComponent(location);
-    var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+module.exports={
+  getTemp:function(location){
+    var encodelocation=encodeURIComponent(location);
+    var requestUrl=`${OPEN_WEATHER_MAP_URL}&q=${encodelocation}`;
 
-    return axios.get(requestUrl).then(function (res) {
-      if (res.data.cod && res.data.message) {
+    return axios.get(requestUrl).then(function(res){
+      if(res.data.cod && res.data.message){
         throw new Error(res.data.message);
-      } else {
+      }else{
         return res.data.main.temp;
       }
-    }, function (res) {
+    }, function(res){
       throw new Error(res.data.message);
     });
   }
